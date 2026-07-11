4ba66c3aef67d86136ed09a676df315fba8ad57f
KrishnaNaik6 <kn670423@gmail.com>
2026-07-11 09:12:53 +0530

    feat: create auth module and authentication DTOs

---
+++ backend/src/modules/auth/dto/register.dto.ts
@@ -0,0 +1,23 @@
+import { IsEmail, IsString, Matches, MaxLength, MinLength } from "class-validator";
+
+export class RegisterDTO {
+    @IsString()
+    @MinLength(3)
+    @MinLength(100)
+    name!: string;
+
+    @IsEmail()
+    email!: string;
+
+    @IsString()
+    @MinLength(8)
+    @MaxLength(50)
+    @Matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/,
+        {
+            message:
+                'Password must contain uppercase, lowercase, number and speciall character',
+        }
+    )
+    password!: string;
+}
